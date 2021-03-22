c8bf84b3a0072a150d3b553ea41dbba68e9f6b1a
Kuriyagawa Takashi <0th59p11323543k@gmail.com>
2021-03-22 07:39:49 +0000

    20210322

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
     #get 'users/new'
   # get 'pages/index'
   root  'pages#index'
   get 'pages/help'
   #get 'pages/link'
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
 end
